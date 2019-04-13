83d1fa489ca400f16b255340157a6cd2af646f40
g2g1 <49556738+g2g1@users.noreply.github.com>
2019-04-13 17:23:18 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+$dbusername = "root";
+$dbpassword = "gg.pass1.phpassass1n.";
+$dbhostname = "localhost";
+$dbname = "login";
+
+$conn = mysqli_connect($dbhostname,$dbusername,$dbpassword,$dbname) or die("error 469, couldnt connect blyat");
+if(!$conn){echo "connection sucks";}
+
+if(isset($_POST['submit'])){
+$username = mysqli_real_escape_string($conn, $_POST['user']);
+$password =  mysqli_real_escape_string($conn, $_POST['pass']);
+
+
+	
+									  
+	$sql = "INSERT INTO users (username, password) VALUES('$username', '$password');";
+	mysqli_query($conn, $sql);
+	echo "success";
+							}else{echo "isset not working";}
+						    
+							
+
+	
+
+
+
+ ?>
